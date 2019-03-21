d08d9f6ee021e84c11051f1b5583aba424641a0d
Henrique Pinheiro da Silva <hps.inf@gmail.com>
2019-03-21 20:44:36 -0300

    bodyparser mudando extended para false

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const config = require('config');
 
 //Middlewares 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Permissões de headers
