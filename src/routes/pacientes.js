8bdb117a23dc3c0070c51ef5c843ef60bb79206a
Henrique Pinheiro da Silva <hps.inf@gmail.com>
2019-03-21 21:00:10 -0300

    add endpoint cadastrar com parametros

--- src/routes/pacientes.js
+++ src/routes/pacientes.js
@@ -7,5 +7,6 @@ const auth = require('../../services/auth');
 
 pacienteRoute.get('/', controller.get);
 pacienteRoute.post('/cadastrar', controller.post);
+pacienteRoute.post('/cadastrar/:nome/:genero/:data_nascimento', controller.post);
 
 module.exports = pacienteRoute;
